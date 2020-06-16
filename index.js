43bc9a79c6b8400d0ba6070d03aba1b5f7f0a993
Juan Manuel Machado <machadojuanmanuel@hotmail.com>
2020-06-16 00:24:59 -0300

    Fixed hiding button on scroll

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 (() => {
-    ScrollReveal().reveal('p, h2, .img', { delay: 350 });
-})()
+  ScrollReveal().reveal("p, h2, .img", { delay: 350 });
+})();
 
 let targets;
+let arrow;
 (() => {
-    targets = [...document.querySelectorAll(".content, .section-intro")] || [];
-})()
+  targets = [...document.querySelectorAll(".content, .section-intro")] || [];
+  arrow = document.querySelector(".fixed-container");
+})();
 
 function scrollNext() {
-    const minHeight = window.innerHeight / 10;
-    const scrollable = targets.find(target => target.getBoundingClientRect().top > minHeight);
-    console.log({ scrollable })
-    if (scrollable) {
-        scrollable.scrollIntoView()
-    }
+  const minHeight = window.innerHeight / 10;
+  const scrollable = targets.find(
+    (target) => target.getBoundingClientRect().top > minHeight
+  );
+  console.log({ scrollable });
+  if (scrollable) {
+    scrollable.scrollIntoView();
+    displayArrowOrNot();
+  }
 }
 
+function displayArrowOrNot() {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
+    arrow.style.display = "none";
+  } else if (arrow.style.display === "none") {
+    arrow.style.display = "block";
+  }
+};
+
+window.addEventListener("scroll", displayArrowOrNot);
