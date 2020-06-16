135ea6350273d055a8e41f4cb4cf91db6591a004
Juan Manuel Machado <machadojuanmanuel@hotmail.com>
2020-06-15 22:25:49 -0300

    First version scrollable button

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 (() => {
-    ScrollReveal().reveal('p, h2, .img, a', { delay: 300 });
+    ScrollReveal().reveal('p, h2, .img', { delay: 300 });
 })()
+
+
+function scrollNext() {
+    const minHeight = window.innerHeight / 10;
+    const targets = [...document.querySelectorAll('.content, .section-intro')] || [];
+    const scrollable = targets.find(target => target.getBoundingClientRect().top > minHeight);
+    console.log({ scrollable })
+    if (scrollable) {
+        scrollable.scrollIntoView()
+    }
+}
+
