34d9dabb414a980616c75bfb79b1aaf3e1edda4d
Juan Manuel Machado <machadojuanmanuel@hotmail.com>
2020-06-15 23:59:33 -0300

    Fixed scroll second version

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 (() => {
-    ScrollReveal().reveal('p, h2, .img', { delay: 300 });
+    ScrollReveal().reveal('p, h2, .img', { delay: 350 });
 })()
 
 let targets;
 (() => {
     targets = [...document.querySelectorAll(".content, .section-intro")] || [];
 })()
-
 
 function scrollNext() {
     const minHeight = window.innerHeight / 10;
